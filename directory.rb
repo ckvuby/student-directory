e6916b17307c2b9a7a78c987c082f83941e78d9e
ckvuby <cyberpunk9000@googlemail.com>
2018-08-23 15:30:17 +0000

    added - cohort to symbol, applys default value if none entered.

--- directory.rb
+++ directory.rb
@@ -2,8 +2,8 @@
 
 # and then print them
 def print_header
-  puts "The students of Villians Academy"
-  puts "---------------"
+  puts "The students of Villians Academy".center(100)
+  puts "---------------".center(100)
 end
 
 def print(students)
@@ -14,7 +14,7 @@ end
 
 # finally, we print the total number of students
 def print_footer(directory)
-  puts "Overall, we have #{directory.count} great students"
+  puts "Overall, we have #{directory.count} great students".center(100)
 end
 
 def input_students
@@ -23,17 +23,23 @@ def input_students
   # create an empty array
   students = []
   #get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
   # while the name is not empty, repeat the code
   while !name.empty? do
     puts "Please enter hobby of student"
-    hobby = gets.chomp
+    hobby = gets.chomp.capitalize
+    puts "Please enter your cohort start month"
+    cohort = gets.chomp.to_sym
+    if cohort.empty?
+      cohort = "August".to_sym
+    end
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobby: hobby}
+    students << {name: name, cohort: cohort, hobby: hobby}
     puts "Now we have #{students.count} students"
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   # returns the array of students
+  puts students
   students
 end
 #nothing happens until we call the methods
