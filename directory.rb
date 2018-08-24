1e894c56cd777d8ddcb6937a4f0548aa9bc15364
ckvuby <cyberpunk9000@googlemail.com>
2018-08-24 15:32:52 +0000

    added load method to load data from students.csv

--- directory.rb
+++ directory.rb
@@ -69,6 +69,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"  
 end
 
@@ -86,8 +87,10 @@ def process(selection)
     @students = input_students            # input the students
   when "2"
     show_students        #  show the students
-  when "3"
+  when "3"              # save the students & cohort to students.csv
     save_students
+  when "4"
+    load_students
   when "9"    
     exit      # this will cause the program to terminate
   else
@@ -107,5 +110,13 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 
 interactive_menu
